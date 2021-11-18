dfa54f993e5e1a2cda55ef6a131a8cd26d134eb7
damansaini12 <58924408+damansaini12@users.noreply.github.com>
2021-11-18 16:47:54 -0700

    Add files via upload

--- design3/js/js.js
+++ design3/js/js.js
@@ -13,8 +13,7 @@ let trans = () => {
 };
 
 
-const fn = getRandomInt(11); //first num
-
+const fn = getRandomInt(1000); //first num
 const op = getRandomInt(3); //operator
 const sn = getRandomInt(11); //second num
 const operators = ["plus", "minus", "times", "divide"];
@@ -39,7 +38,10 @@ console.log(fn);
     // const path3 = "audio/" + lang + "/" + sn + ".mp3";
     // const audio3 = new Audio(path3);
 
-    audio.play();
+    window.setTimeout(() => {
+      audio.play();
+    }, 100);
+
     // setTimeout(() => {
     //     audio2.play();
     // }, 1500);
@@ -177,5 +179,67 @@ function resetInputColorCorrect(id) {
 
 function showAnswer() // no ';' here
 {
-    document.getElementById("answer").innerHTML = fn;
+    document.getElementById("answer").innerHTML = fn + " = " +  numberToWords(fn);
 }
+
+var numberToWords = function(num) {
+    if (num === 0) return 'Zero'
+
+    let finalString = [];
+
+    [...numberToWordMap.keys()].reverse().map(key => {
+        if (num >= key) {
+            if (key >= 100) {
+                const numberOfTimesKeyFitsNum = Math.floor(num / key);
+                const number = memoize[numberOfTimesKeyFitsNum] ? memoize[numberOfTimesKeyFitsNum] : numberToWords(numberOfTimesKeyFitsNum);
+                if (!memoize[numberOfTimesKeyFitsNum]) memoize[numberOfTimesKeyFitsNum] = number;
+
+                finalString.push(number + ' ' + numberToWordMap.get(key) + ' ')
+            }
+            else {
+                finalString.push(numberToWordMap.get(key) + ' ')
+            }
+
+            num = num % key;
+        }
+    });
+
+    finalString = finalString.join('');
+
+    return finalString.slice(0, finalString.length - 1);
+};
+
+let memoize = {};
+
+let numberToWordMap = new Map();
+numberToWordMap.set(1, 'One')
+numberToWordMap.set(2, 'Two')
+numberToWordMap.set(3, 'Three')
+numberToWordMap.set(4, 'Four')
+numberToWordMap.set(5, 'Five')
+numberToWordMap.set(6, 'Six')
+numberToWordMap.set(7, 'Seven')
+numberToWordMap.set(8, 'Eight')
+numberToWordMap.set(9, 'Nine')
+numberToWordMap.set(10, 'Ten')
+numberToWordMap.set(11, 'Eleven')
+numberToWordMap.set(12, 'Twelve')
+numberToWordMap.set(13, 'Thirteen')
+numberToWordMap.set(14, 'Fourteen')
+numberToWordMap.set(15, 'Fifteen')
+numberToWordMap.set(16, 'Sixteen')
+numberToWordMap.set(17, 'Seventeen')
+numberToWordMap.set(18, 'Eighteen')
+numberToWordMap.set(19, 'Nineteen')
+numberToWordMap.set(20, 'Twenty')
+numberToWordMap.set(30, 'Thirty')
+numberToWordMap.set(40, 'Forty')
+numberToWordMap.set(50, 'Fifty')
+numberToWordMap.set(60, 'Sixty')
+numberToWordMap.set(70, 'Seventy')
+numberToWordMap.set(80, 'Eighty')
+numberToWordMap.set(90, 'Ninety')
+numberToWordMap.set(100, 'Hundred')
+numberToWordMap.set(1000, 'Thousand')
+numberToWordMap.set(1000000, 'Million')
+numberToWordMap.set(1000000000, 'Billion')
